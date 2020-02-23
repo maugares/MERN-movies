220918ba19dd5fad9728e38a27ca7c176818255f
Mauro Garcia Esteban <mauro.garcia.esteban@diymaxeda.com>
2020-02-23 17:36:50 +0100

    Add the router to the server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 
 import { db } from './db'
+import { router as movieRouter } from './routes/movie-router'
 
 const app = express()
 const apiPort = 3000
@@ -16,5 +17,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
   res.send('Hi Bernie. I am Mauro')
 })
+
+app.use('/api', movieRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -2,11 +2,10 @@ import express from 'express'
 
 import { createMovie, getMovie, updateMovie, deleteMovie, getMovieById } from '../controllers/movie-ctrl'
 
-const router = express.Router()
+export const router = express.Router()
 
 router.post('/movie', createMovie)
 router.get('/movie', getMovie)
 router.put('/movie/:id', updateMovie)
 router.delete('/movie/:id', deleteMovie)
 router.get('/movie/:id', getMovieById)
- 
