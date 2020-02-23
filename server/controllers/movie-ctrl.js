4890a50e5b82df36065748a5e4daf7ffdd2b24c3
Mauro Garcia Esteban <mauro.garcia.esteban@diymaxeda.com>
2020-02-23 17:10:26 +0100

    Add update route

--- server/controllers/movie-ctrl.js
+++ server/controllers/movie-ctrl.js
@@ -41,8 +41,47 @@ export const readMovie = async (req, res) => {
       return res.status(400).json({ success: false, error: err })
     }
     if (!movies.length) {
-      return res.status(404).json({ success: false, error: 'Movie not found' })
+      return res.status(404).json({ success: false, error: 'Movies not found' })
     }
     return res.status(200).json({ success: true, data: movies })
   }).catch(err => console.log(err))
+}
+
+// crUd - Update
+export const updateMovie = async (req, res, err) => {
+  const body = req.body
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      message: 'You must provide a body to update'
+    })
+  }
+
+  Movie.findOne({ _id: req.params.id }, (err, movie) => {
+    if (err) {
+      return res.status(404).json({
+        err,
+        message: 'Movie not found!'
+      })
+    }
+    movie.name = body.name
+    movie.time = body.time
+    movie.rating = body.rating
+    movie
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: movie._id,
+          message: 'Movie updated!'
+        })
+      })
+      .catch(error => {
+        return res.status(404).json({
+          error,
+          message: 'Movie not updated'
+        })
+      })
+  })
 }
--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -1,9 +1,10 @@
 import express from 'express'
 
-import { createMovie, readMovie } from '../controllers/movie-ctrl'
+import { createMovie, readMovie, updateMovie } from '../controllers/movie-ctrl'
 
 const router = express.Router()
 
 router.post('/movie', createMovie)
 router.get('/movie', readMovie)
+router.put('/movie/:id', updateMovie)
  
