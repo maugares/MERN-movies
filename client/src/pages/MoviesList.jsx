b6c32ea957ca51d6bdacfd0bed8c765bd329ffb9
Mauro Garcia Esteban <mauro.garcia.esteban@diymaxeda.com>
2020-02-23 19:55:48 +0100

    Create page MoviesList

---
+++ client/src/pages/MoviesList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MoviesList extends Component {
+  render() {
+    return (
+      <div>
+        <p>In this page you'll see the list of movies</p>
+      </div>
+    )
+  }
+}
